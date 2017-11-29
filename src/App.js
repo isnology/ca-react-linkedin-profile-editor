63f23679e8e78ce46725f5305d9c0c63bb18fc4d
IsNology <glenn@isnology.com>
2017-11-29 12:20:10 +1100

    initial commit with hardcoded layout and first/last name editable

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    user: {
+      firstName: 'Alice',
+      lastName: 'Jones',
+      profileImage: 'https://randomuser.me/api/portraits/lego/1.jpg'
+    }
+  }
+
+  onChangeName = (newValue, name) => {
+    this.setState((prevState) => {
+      const newUser = { ...prevState.user, [name]: newValue }
+      return {
+        user: newUser
+      }
+    })
+  }
+
+  render() {
+    const user = this.state.user
+
+    return (
+      <div className="App">
+        <h1>Linkedin Profile Editor</h1>
+        <img src={ user.profileImage } />
+        <p>Name: { user.firstName } { user.lastName }</p>
+
+        <label>
+          First name:
+          {' '}
+          <input type="text" value={ user.firstName }
+            onChange={ (event) => {
+                this.onChangeName(event.target.value, 'firstName')
+              }
+            }
+          />
+        </label>
+        <label>
+          Last name:
+          {' '}
+          <input type="text" value={ user.lastName }
+            onChange={ (event) => {
+                this.onChangeName(event.target.value, 'lastName')
+              }
+            }
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default App;
